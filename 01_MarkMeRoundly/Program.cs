79085bf08a459b4f83d240ed89443debda22dcdf
Konstantins Romanovs <konstantin.hvac@gmail.com>
2021-10-04 18:42:41 +0300

    Adds simple logic for rounding the input

---
+++ 01_MarkMeRoundly/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _01_MarkMeRoundly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input = "23, 65, 76, 78, 23, 43";
+            
+            int[] arr = Array.ConvertAll(input.Split(","), s => int.Parse(s.Trim()));
+
+            int[] outArr = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var remainder = arr[i] % 5;
+                if (arr[i]! < 40 || remainder == 0 || remainder < 3)
+                {
+                    outArr[i] = arr[i];
+                }
+                else
+                {
+                    var a = arr[i] - remainder + 5;
+                    outArr[i] = a;
+                }
+
+
+            }
+
+
+
+            for (int i = 0; i < outArr.Length; i++)
+            {
+                Console.WriteLine(outArr[i]);
+            }
+
+
+        }
+
+
+    }
+
+
+}
